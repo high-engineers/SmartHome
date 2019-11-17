0c3c6db7a77464d4bf34f0d3cd3342b93797b943
Bartosz Madej <johnnythemadman@gmail.com>
2019-11-17 16:25:32 +0100

    Added new migration: rooms have Type now.

---
+++ server/SmartHome.Data/Migrations/20191117152351_AddedRoomType.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartHome.Data.Migrations
+{
+    public partial class AddedRoomType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Room",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Room");
+        }
+    }
+}
