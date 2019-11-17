86e23d63ff1daed9725212671ef7b1346957703a
Bartosz Madej <johnnythemadman@gmail.com>
2019-11-17 17:52:10 +0100

    test purposes

---
+++ server/SmartHome.API/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartHome.API
+{
+    public class ValuesController
+    {
+        [HttpGet("api/values")]
+        public IList<string> Get()
+        {
+            return new List<string> { "walju1", "walju2" };
+        }
+    }
+}
