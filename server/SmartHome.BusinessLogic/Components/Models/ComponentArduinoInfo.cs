bea1e83b351b746a6dd8eb825df6b601310f6e74
Bartosz Madej <johnnythemadman@gmail.com>
2019-11-23 15:46:59 +0100

    GetAllComponents endpoint for arduino.

---
+++ server/SmartHome.BusinessLogic/Components/Models/ComponentArduinoInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SmartHome.BusinessLogic.Components.Models
+{
+    public class ComponentArduinoInfo
+    {
+        public Guid Id { get; set; }
+        public string State { get; set; }
+    }
+}
---
+++ server/SmartHome.BusinessLogic/Components/Queries/GetAllComponentsQuery.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SmartHome.BusinessLogic.Components.Queries
+{
+    public class GetAllComponentsQuery
+    {
+        public Guid SmartHomeEntityId { get; set; }
+        public Guid RoomId { get; set; }
+    }
+}
