5a5e505cd5e9af56d884783424d6b3f3d0a66f96
Bartosz Madej <johnnythemadman@gmail.com>
2019-11-24 18:42:58 +0100

    RegisterUser endpoint. (api/users/register)

---
+++ server/SmartHome.API/Infrastructure/PostModels/UserRegisterCredentialsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartHome.API.Infrastructure.PostModels
+{
+    public class UserRegisterCredentialsModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ server/SmartHome.BusinessLogic/ValidationRules/UsernameDoesntExistValidationRule.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using SmartHome.BusinessLogic.Infrastructure.Models;
+using SmartHome.Data;
+using System.Threading.Tasks;
+
+namespace SmartHome.BusinessLogic.ValidationRules
+{
+    public class UsernameDoesntExistValidationRule : IValidationRule<string>
+    {
+        private const string UsernameExistsErrorMessage = "UsernameExistsError";
+
+        private readonly SmartHomeContext _context;
+
+        public UsernameDoesntExistValidationRule(SmartHomeContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IResult<object>> ValidateAsync(string username)
+        {
+            var result = await _context.Users.AnyAsync(x => x.Username == username);
+
+            return result
+                ? Result<object>.Fail(new ResultError(StatusCodes.Status409Conflict, UsernameExistsErrorMessage))
+                : Result<object>.Success();
+        }
+    }
+
+}
