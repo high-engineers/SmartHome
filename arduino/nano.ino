2b8e88c0f2d336812769dcc929183186651cbcc4
Wankowicz <milosz.wankowicz@grupawp.pl>
2019-11-26 20:37:46 +0100

    a lot of spaghetti code

--- arduino/nano.ino
+++ arduino/nano.ino
@@ -1,36 +1,36 @@
-
-#define MOTION_IN 5
-#define MOTION_OUT 10
-
-long timer = 0;
-long motionTime = 4000;
+//
+//#define MOTION_IN 5
+//#define MOTION_OUT 10
+//
+//long timer = 0;
+//long motionTime = 4000;
 
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(MOTION_IN, INPUT);
-  pinMode(MOTION_OUT, OUTPUT);
+//  // put your setup code here, to run once:
+  //Serial.begin(9600);
+ // pinMode(MOTION_IN, INPUT);
+//  pinMode(MOTION_OUT, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(digitalRead(MOTION_IN) == HIGH){
-    Serial.println("WYKRYŁEM RUCH!");
-    digitalWrite(MOTION_OUT, HIGH);
-   // timer = millis();
-    delay(200);
-  } else {
-    digitalWrite(MOTION_OUT, LOW);
-    delay(200);
-  }
-
-  if(digitalRead(MOTION_OUT) == HIGH){
-    Serial.print("Przesyłam HIGH");
-  } else {
-    Serial.println("Przesyłam LOW");
-  }
-  delay(100);
+  //if(digitalRead(MOTION_IN) == HIGH){
+   // Serial.println("WYKRYŁEM RUCH!");
+ //   digitalWrite(MOTION_OUT, HIGH);
+//   // timer = millis();
+   // delay(200);
+//  } else {
+//    digitalWrite(MOTION_OUT, LOW);
+  //  Serial.println("NIE");
+   // delay(200);
+ // }
+//  if(digitalRead(MOTION_OUT) == HIGH){
+//    Serial.print("Przesyłam HIGH");
+//  } else {
+//    Serial.println("Przesyłam LOW");
+//  }
+//  delay(100);
 //  if ( millis() - timer > motionTime){
 //    digitalWrite(MOTION_OUT, LOW);
 //  } else {
