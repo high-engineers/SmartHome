f0ba97d9ca1349942ad0b72ba4b8565526f615e6
Bartosz Madej <johnnythemadman@gmail.com>
2019-12-02 01:51:47 +0100

    Added validation for GetRoomId (if component has no room assignned it will throw 400 instead of unexpected 500). GetRoomById returns data only from current day.

---
+++ server/SmartHome.BusinessLogic/ValidationRules/ComponentHasRoomAssignedValidationRule.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using SmartHome.BusinessLogic.Infrastructure.Models;
+using SmartHome.Data;
+using System;
+using System.Threading.Tasks;
+
+namespace SmartHome.BusinessLogic.ValidationRules
+{
+    public class ComponentHasRoomAssignedValidationRule : IValidationRule<Guid>
+    {
+        private const string _componentHasNoRoomAssignedErrorMessage = "ComponentHasNoRoomAssignedError";
+        private readonly SmartHomeContext _context;
+
+        public ComponentHasRoomAssignedValidationRule(SmartHomeContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IResult<object>> ValidateAsync(Guid componentId)
+        {
+            var component = await _context.Components.FirstOrDefaultAsync(x => x.ComponentId == componentId);
+
+            return component.RoomId.HasValue
+                ? Result<object>.Success()
+                : Result<object>.Fail(new ResultError(StatusCodes.Status400BadRequest, _componentHasNoRoomAssignedErrorMessage));
+        }
+    }
+}
