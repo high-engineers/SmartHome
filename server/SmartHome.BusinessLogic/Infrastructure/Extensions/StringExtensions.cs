5dbf56798284e1a83f26b61e1067951e36742011
Bartosz Madej <johnnythemadman@gmail.com>
2019-12-05 00:44:39 +0100

    SHA256 Hashing.

---
+++ server/SmartHome.BusinessLogic/Infrastructure/Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SmartHome.BusinessLogic.Infrastructure.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string GetHashedString(this string obj)
+        {
+            using (var hash = SHA256.Create())
+            {
+                var bytes = Encoding.UTF8.GetBytes(obj);
+                var hashed = hash.ComputeHash(bytes);
+                return string.Join(
+                    string.Empty,
+                    hashed.Select(x => x.ToString("x2")));
+            }
+        }
+    }
+}
